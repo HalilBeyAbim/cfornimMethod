969d97bd60e659e1756f8d4cf833b48612e2f8fc
HalilBeyAbim <khalilbaygkh@code.edu.az>
2022-07-18 00:08:22 +0400

    first commit

---
+++ assecst/JS/main.js
@@ -0,0 +1,11 @@
+
+
+var sure = confirm("do you want to visit the site?");
+ if(sure == true){
+    document.body.style.backgroundColor = 'green'
+ }
+ else{
+    document.write('404').style = 'blue'
+ }
+
+ 
